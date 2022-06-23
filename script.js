50700bda4dde01828ad3ea45782116e8d2a5d124
Moses Michael <mosesmichael270@gmail.com>
2022-06-23 15:03:38 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let firstName = 'Michael';
+let lastName = 'Moses';
+let fullName = "firstName + lastName";
+
+alert(firstName +" "+ lastName);
+let height = '5,9ft';
+let country = 'Nigerian';
+
+alert(height);
+alert(country);
